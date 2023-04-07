58513de3245e3f5289ef391af14b99cd33e3edee
Abdishakur Fifa <85757306+abdishakur1000@users.noreply.github.com>
2023-04-07 09:16:07 +0000

    login aan u sameeyay

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, redirect
+from flask_login import LoginManager, UserMixin
+
+login_manager = LoginManager()
 
 app = Flask(__name__)
 
@@ -6,6 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
 
 @app.route('/services')
@@ -18,31 +26,42 @@ def products():
   return render_template('products.html')
 
 
-@app.route('/best_product')
-def best_product():
-  return render_template('best_product.html')
+@app.route('/bestproduct')
+def bestproduct():
+  return render_template('bestproduct.html')
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup')
 def signup():
-  if request.method == 'POST':
-    # process user sign up data
-    return redirect(url_for('login'))
   return render_template('signup.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    # process user login data
-    return redirect(url_for('home'))
-  return render_template('login.html')
+    # Handle user login
+    username = request.form['username']
+    password = request.form['password']
+    # Check if username and password are correct
+    if username == 'example' and password == 'password':
+      return redirect('/')
+    else:
+      return render_template('login.html', error=True)
+  else:
+    # Show login form
+    return render_template('login.html', error=False)
 
 
-@app.route('/image')
-def image():
-  return render_template('image.html')
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+
+class User(UserMixin):
+
+  def __init__(self, id):
+    self.id = id
 
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(host='0.0.0.0', port=5000, debug=True)
